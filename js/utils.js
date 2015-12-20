885dcb50a08a927b8f6026aa16b468d3d8a5e0f1
Dan Schaper <dschaper@ganymeade.com>
2015-12-19 23:17:25 -0800

    Modify utils.js for customization

--- js/utils.js
+++ js/utils.js
@@ -1,10 +1,9 @@
 jQuery(function($)
 {
-    $("#contact_form").submit(function()
+    $("#signup").submit(function()
     {
         var email = $("#email").val(); // get email field value
         var name = $("#name").val(); // get name field value
-        var msg = $("#msg").val(); // get message field value
         $.ajax(
         {
             type: "POST",
@@ -18,7 +17,6 @@ jQuery(function($)
                         'Reply-To': email
                     },
                     'subject': 'Website Contact Form Submission',
-                    'text': msg,
                     'to': [
                     {
                         'email': 'launch.control@ganymeade.com',
@@ -32,7 +30,6 @@ jQuery(function($)
             alert('Your message has been sent. Thank you!'); // show success message
             $("#name").val(''); // reset field after successful submission
             $("#email").val(''); // reset field after successful submission
-            $("#msg").val(''); // reset field after successful submission
         })
         .fail(function(response) {
             alert('Error sending message.');
